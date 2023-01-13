f9df67054c51ba351511b4734d66c935beb47fd6
aodincov <aodincov@mmtr.ru>
2023-01-13 16:36:26 +0300

    add Table read operations

---
+++ src/main/java/ru/mmtr/at/hbase/connection/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package ru.mmtr.at.hbase.connection;
+
+import lombok.SneakyThrows;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class ConnectionFactory {
+
+    @SneakyThrows
+    public static Connection getConnection() {
+        Configuration conf = HBaseConfiguration.create();
+        conf.clear();
+        //todo вынести в .properties
+        conf.set("hbase.zookeeper.quorum", "hbase-docker");
+        conf.set("hbase.zookeeper.property.clientPort","2181");
+        conf.set("zookeeper.znode.parent", "/hbase");
+        conf.set("hbase.master", "127.0.0.1:16000");
+
+        conf.set("hbase.client.retries.number", "3");  // default 35
+        conf.set("hbase.rpc.timeout", "10000");  // default 60 secs
+        conf.set("hbase.rpc.shortoperation.timeout", "5000"); // default 10 secs
+
+        final Configuration configuration = HBaseConfiguration.create(conf);
+
+        HBaseAdmin.available(configuration);
+
+        return org.apache.hadoop.hbase.client.ConnectionFactory.createConnection(conf);
+
+    }
+}
---
+++ src/main/java/ru/mmtr/at/hbase/interfaces/TableOperations.java
@@ -0,0 +1,95 @@
+package ru.mmtr.at.hbase.interfaces;
+
+import lombok.SneakyThrows;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.util.Bytes;
+import ru.mmtr.at.hbase.connection.ConnectionFactory;
+
+import java.util.*;
+
+public class TableOperations {
+
+    /***
+     *
+     * @param c коннект к бд. смотри {@link ConnectionFactory#getConnection()}
+     * @param tableName имя таблицы в БД
+     * @param rowKeyName первичный ключ таблицы
+     * @param columnFamilyName имя семейства столбцов (Column Family)
+     * @param columnName имя столбца (параметра) ВНУТРИ семества столбцов (Column\Qualifier)
+     * @return все значения Qualifier, отсортированные по timestamp от новых к старым. K - Timestamp (UNIX Time), V - данные в виде Byte[]
+     */
+
+    @SneakyThrows
+    public static Map<Long, byte[]> getAllVersionsFromTable(
+            Connection c,
+            String tableName,
+            String rowKeyName,
+            String columnFamilyName,
+            String columnName
+    ) {
+        return new HashMap<>(getQualifiersRawData(c, tableName, rowKeyName, columnFamilyName).get(Bytes.toBytes(columnName)));
+    }
+
+    /***
+     *  *
+     * @param c коннект к бд. смотри {@link ConnectionFactory#getConnection()}
+     * @param tableName имя таблицы в БД
+     * @param rowKeyName первичный ключ таблицы
+     * @param columnFamilyName имя семейства столбцов (Column Family)
+     *
+     * @return содержимое пересечения RowKey и ColumnFamily (все Columns\Qualifier)
+     */
+
+    @SneakyThrows
+    private static NavigableMap<byte[], NavigableMap<Long, byte[]>> getQualifiersRawData(
+            Connection c,
+            String tableName,
+            String rowKeyName,
+            String columnFamilyName
+    ) {
+
+        Get getQuery = new Get(Bytes.toBytes(rowKeyName));
+        getQuery.readAllVersions();
+
+        NavigableMap<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> allVersions =
+                c.getTable(TableName.valueOf(tableName)).get(getQuery).getMap();
+
+        return allVersions.get(Bytes.toBytes(columnFamilyName));
+    }
+
+    /***
+     *
+     * @param qualifiers содержимое пересечения RowKey и ColumnFamily (все Columns\Qualifier)
+     * @param columnName имя столбца (параметра) ВНУТРИ семества столбцов (Column\Qualifier)
+     * @return все значения Qualifier, отсортированные по timestamp от новых к старым. K - Timestamp (UNIX Time), V - данные в виде Byte[]
+     */
+    @SneakyThrows
+    private static NavigableMap<Long, byte[]> getSingleQualifierRawData(
+            NavigableMap<byte[], NavigableMap<Long, byte[]>> qualifiers,
+            String columnName
+    ) {
+        return qualifiers.get(Bytes.toBytes(columnName));
+    }
+
+    /***
+     *
+     * @param c коннект к бд. смотри {@link ConnectionFactory#getConnection()}
+     * @param tableName имя таблицы в БД
+     * @param rowKeyName первичный ключ таблицы
+     * @param columnFamilyName имя семейства столбцов (Column Family)
+     * @param columnName имя столбца (параметра) ВНУТРИ семества столбцов (Column\Qualifier)
+     * @return все значения Qualifier, отсортированные по timestamp от новых к старым. K - Timestamp (UNIX Time), V - данные в виде Byte[]
+     */
+    @SneakyThrows
+    private static NavigableMap<Long, byte[]> getSingleQualifierRawData(
+            Connection c,
+            String tableName,
+            String rowKeyName,
+            String columnFamilyName,
+            String columnName
+    ) {
+        return getSingleQualifierRawData(getQualifiersRawData(c, tableName, rowKeyName, columnFamilyName), columnName);
+    }
+}
