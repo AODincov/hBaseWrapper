5e54486890a7e3354ff262d7f6526a490e49e6d6
aodincov <aodincov@mmtr.ru>
2023-01-16 17:49:56 +0300

    Add ColumnLink

---
+++ src/main/java/ru/mmtr/at/hbase/dataHelpers/ColumnLink.java
@@ -0,0 +1,20 @@
+package ru.mmtr.at.hbase.dataHelpers;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/***
+ * {@link ColumnLinkBuilder}
+ */
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
+public class ColumnLink {
+
+    String tableName;
+    String rowKeyName;
+    String columnFamilyName;
+    String columnName;
+
+}
